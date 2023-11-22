4448eac2c349631805dbb839f13cc894fa469452
ankit gupta <ankitgupta17878@gmail.com>
2023-11-22 19:24:39 +0530

    get users data

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express =require('express');
-const users =require('./MOCK_DATA.json');
+const express = require('express');
+const users = require('./MOCK_DATA.json');
 
-const app=express();
-const PORT=8000;
+const app = express();
+const PORT = 8000;
 
-app.get("/users",(req,res)=>{
+app.get("/users", (req, res) => {
     return res.json(users);
 })
 
 
-app.listen(PORT,()=>{
-    console.log("server run on port no",PORT);
+app.listen(PORT, () => {
+    console.log("server run on port no", PORT);
 })
