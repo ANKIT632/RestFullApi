f2f40532ca48ebd3d3bd87d9e8545249373792dd
ankit gupta <ankitgupta17878@gmail.com>
2023-11-23 23:28:33 +0530

    get users data

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const users = require('./MOCK_DATA.json');
 const app = express();
 const PORT = 8000;
 
-app.get("/users", (req, res) => {
+app.get("/api/users", (req, res) => {
     return res.json(users);
 })
+
+
+//Dynamic path param
+app.get("/api/users/:id", (req, res) => {
+   const id=Number(req.params.id);
+   const user=users.find((user)=>user.id===id);
+   return res.json(user);
+})
+
+app
 
 
 app.listen(PORT, () => {
