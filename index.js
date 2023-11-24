a8ad4bd872548e3736c3746e0f96ef3189213da7
ankit gupta <ankitgupta17878@gmail.com>
2023-11-24 23:19:31 +0530

    complete post method

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
-
+const fs = require('fs');
 const app = express();
 const PORT = 8000;
+
+// use middleware handle data which is generate from client side.
+app.use(express.urlencoded({ extended: false }));
 
 // On same route handle multiple request.
 // use dynamic path  
@@ -23,8 +26,17 @@ app.get("/api/users", (req, res) => {
 })
 
 app.post("/api/users", (req, res) => {
-    return res.json({ status: "padding" });
-})
+    const body = req.body;
+    users.push({ id: users.length + 1, ...body });
+
+    //add new file with update data
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "success", id: users.length });
+    });
+
+    fs.close();
+
+});
 
 
 
