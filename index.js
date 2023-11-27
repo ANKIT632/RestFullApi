a3250d1cf836adf3caa625335e2e31a6572af3b6
ankit gupta <ankitgupta17878@gmail.com>
2023-11-27 22:44:48 +0530

    complete post method

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const users = require('./MOCK_DATA.json');
+const express = require('express')
+const users = require('./Data.json');
 const fs = require('fs');
 const app = express();
 const PORT = 8000;
 
+
 // use middleware handle data which is generate from client side.
 app.use(express.urlencoded({ extended: false }));
+
+//add custom middleware which maintain log
+
+
+app.use((req,res,next)=>{
+   fs.appendFile('log.txt',`\n${new Date(Date.now() + (new Date() - Date.now()))} : ${req.ip}  ${req.method} : ${req.path}`,(err,data)=>{
+    next();
+   });
+  
+});
+
 
 // On same route handle multiple request.
 // use dynamic path  
@@ -14,7 +26,13 @@ app.route("/api/users/:id").get((req, res) => {
     const user = users.find((user) => user.id === id);
     return res.json(user);
 })
+
+     //Edit the user.
     .patch((req, res) => {
+        const id= req.params.id;
+        const body =req.body;
+        
+
         return res.json({ status: "padding" });
     })
     .delete((req, res) => {
@@ -26,15 +44,15 @@ app.get("/api/users", (req, res) => {
 })
 
 app.post("/api/users", (req, res) => {
+  
     const body = req.body;
     users.push({ id: users.length + 1, ...body });
 
     //add new file with update data
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    fs.writeFile("./Data.json", JSON.stringify(users), (err, data) => {
         return res.json({ status: "success", id: users.length });
     });
 
-    fs.close();
 
 });
 
